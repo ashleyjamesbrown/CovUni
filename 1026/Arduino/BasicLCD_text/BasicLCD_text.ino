5616099ef258833f0291dbdd42aefbfe26a170f4
Ashley James Brown <me@ashleyjamesbrown.com>
2018-03-22 15:39:39 +0000

    week 9

---
+++ 1026/Arduino/BasicLCD_text/BasicLCD_text.ino
@@ -0,0 +1,14 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+void setup() {
+  lcd.begin(16, 2); //start the display
+  lcd.print("cov uni"); //start print
+}
+
+
+void loop() {
+
+}
